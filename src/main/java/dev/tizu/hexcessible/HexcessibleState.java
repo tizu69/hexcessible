615b5f97cc7265c301111a981f7287a5e3ba4be9
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-10-23 21:30:11 +0200

    move state into state class

---
+++ src/main/java/dev/tizu/hexcessible/HexcessibleState.java
@@ -0,0 +1,9 @@
+package dev.tizu.hexcessible;
+
+public class HexcessibleState {
+    public static boolean shouldPresent = false;
+    public static String query = "";
+
+    private HexcessibleState() {
+    }
+}
--- src/main/java/dev/tizu/hexcessible/mixin/CastingInterfaceMixin.java
+++ src/main/java/dev/tizu/hexcessible/mixin/CastingInterfaceMixin.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import at.petrak.hexcasting.client.gui.GuiSpellcasting;
+import dev.tizu.hexcessible.HexcessibleState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.text.Text;
@@ -14,29 +15,28 @@ import net.minecraft.util.Formatting;
 
 @Mixin(GuiSpellcasting.class)
 public class CastingInterfaceMixin {
-    private boolean shouldPresent = false;
-    private String query = "";
 
     @Inject(at = @At(value = "INVOKE", target = "play", shift = At.Shift.BEFORE), method = "drawStart", remap = false)
     private void drawStart(double mxOut, double myOut, CallbackInfoReturnable<Boolean> info) {
-        shouldPresent = true;
+        HexcessibleState.shouldPresent = true;
     }
 
     @Inject(at = @At("HEAD"), method = "mouseMoved", remap = false)
     private void mouseMoved(CallbackInfo info) {
-        if (!shouldPresent)
+        if (!HexcessibleState.shouldPresent)
             return;
-        shouldPresent = false;
+        HexcessibleState.shouldPresent = false;
     }
 
     @Inject(at = @At("RETURN"), method = "render", remap = false)
     private void render(DrawContext ctx, int mouseX, int mouseY, float delta,
             CallbackInfo info) {
-        if (!shouldPresent)
+        if (!HexcessibleState.shouldPresent)
             return;
         var tr = MinecraftClient.getInstance().textRenderer;
 
-        var text = !query.equals("") ? Text.literal(query)
+        var text = !HexcessibleState.query.equals("")
+                ? Text.literal(HexcessibleState.query)
                 : Text.translatable("hexcessible.start_typing")
                         .formatted(Formatting.DARK_GRAY, Formatting.ITALIC);
         ctx.drawTooltip(tr, text, mouseX, mouseY);
