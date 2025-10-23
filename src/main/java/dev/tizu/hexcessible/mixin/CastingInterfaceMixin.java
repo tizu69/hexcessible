7f8e741a353e734d25f9b84f3c4c4a5474238711
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-10-23 22:24:50 +0200

    Text input

--- src/main/java/dev/tizu/hexcessible/mixin/CastingInterfaceMixin.java
+++ src/main/java/dev/tizu/hexcessible/mixin/CastingInterfaceMixin.java
@@ -1,5 +1,9 @@
 package dev.tizu.hexcessible.mixin;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,6 +14,7 @@ import at.petrak.hexcasting.client.gui.GuiSpellcasting;
 import dev.tizu.hexcessible.HexcessibleState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
@@ -40,5 +45,41 @@ public class CastingInterfaceMixin {
                 : Text.translatable("hexcessible.start_typing")
                         .formatted(Formatting.DARK_GRAY, Formatting.ITALIC);
         ctx.drawTooltip(tr, text, mouseX, mouseY);
+    }
+
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        var ctrl = (modifiers & GLFW.GLFW_MOD_CONTROL) != 0;
+        switch (keyCode) {
+            case GLFW.GLFW_KEY_BACKSPACE:
+                var q = HexcessibleState.getQuery();
+                if (ctrl) { // remove last word
+                    var words = q.split(" ");
+                    var output = Arrays.stream(words)
+                            .limit(words.length - 1l)
+                            .collect(Collectors.joining(" "));
+                    HexcessibleState.setQuery(output);
+                } else { // remove single character
+                    HexcessibleState.setQuery(q.isEmpty() ? ""
+                            : q.substring(0, q.length() - 1));
+                }
+                break;
+            case GLFW.GLFW_KEY_ESCAPE:
+                if (HexcessibleState.getShouldPresent())
+                    HexcessibleState.setShouldPresent(false);
+                else
+                    ((GuiSpellcasting) (Object) this).close();
+                break;
+            case GLFW.GLFW_KEY_ENTER, GLFW.GLFW_KEY_KP_ENTER, GLFW.GLFW_KEY_TAB:
+                // TODO: submit spellcasting query
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    public boolean charTyped(char chr, int modifiers) {
+        HexcessibleState.setQuery(HexcessibleState.getQuery() + chr);
+        return true;
     }
 }
