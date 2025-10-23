c2e18245fd117e9cb389185494cd2b3e505e0d2f
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-10-23 21:14:50 +0200

    "Start typing" tooltip

--- src/main/java/dev/tizu/hexcessible/mixin/CastingInterfaceMixin.java
+++ src/main/java/dev/tizu/hexcessible/mixin/CastingInterfaceMixin.java
@@ -3,18 +3,22 @@ package dev.tizu.hexcessible.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import at.petrak.hexcasting.client.gui.GuiSpellcasting;
-import dev.tizu.hexcessible.Hexcessible;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
 @Mixin(GuiSpellcasting.class)
 public class CastingInterfaceMixin {
     private boolean shouldPresent = false;
+    private String query = "";
 
     @Inject(at = @At(value = "INVOKE", target = "play", shift = At.Shift.BEFORE), method = "drawStart", remap = false)
     private void drawStart(double mxOut, double myOut, CallbackInfoReturnable<Boolean> info) {
-        Hexcessible.LOGGER.info("Now presenting interface");
         shouldPresent = true;
     }
 
@@ -22,7 +26,19 @@ public class CastingInterfaceMixin {
     private void mouseMoved(CallbackInfo info) {
         if (!shouldPresent)
             return;
-        Hexcessible.LOGGER.info("Done presenting interface");
         shouldPresent = false;
+    }
+
+    @Inject(at = @At("RETURN"), method = "render", remap = false)
+    private void render(DrawContext ctx, int mouseX, int mouseY, float delta,
+            CallbackInfo info) {
+        if (!shouldPresent)
+            return;
+        var tr = MinecraftClient.getInstance().textRenderer;
+
+        var text = !query.equals("") ? Text.literal(query)
+                : Text.translatable("hexcessible.start_typing")
+                        .formatted(Formatting.DARK_GRAY, Formatting.ITALIC);
+        ctx.drawTooltip(tr, text, mouseX, mouseY);
     }
 }
