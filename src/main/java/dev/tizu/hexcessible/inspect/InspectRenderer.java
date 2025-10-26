cf7445b099e8e197dd5d71e593300e2fa7302c48
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-10-26 12:48:47 +0100

    render docs for mouseinspect

--- src/main/java/dev/tizu/hexcessible/inspect/InspectRenderer.java
+++ src/main/java/dev/tizu/hexcessible/inspect/InspectRenderer.java
@@ -1,16 +1,14 @@
 package dev.tizu.hexcessible.inspect;
 
-import java.util.function.Function;
-
-import org.joml.Vector4i;
+import java.util.ArrayList;
 
 import at.petrak.hexcasting.api.casting.math.HexCoord;
 import at.petrak.hexcasting.api.casting.math.HexPattern;
-import dev.tizu.hexcessible.Hexcessible;
+import dev.tizu.hexcessible.PatternEntries;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.text.Text;
-import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.Formatting;
 
 class InspectRenderer {
     private InspectRenderer() {
@@ -24,6 +22,15 @@ class InspectRenderer {
             return;
 
         ctx.drawTooltip(tr, Text.literal(pattern), mx, my);
+
+        var entry = PatternEntries.INSTANCE.getFromSig(pattern);
+        if (entry == null)
+            return;
+        var subtext = new ArrayList<Text>();
+        subtext.add(Text.literal(entry.toString()).formatted(Formatting.BLUE));
+        for (var impl : entry.impls())
+            subtext.add(Text.literal(impl.getArgs()).formatted(Formatting.DARK_GRAY));
+        ctx.drawTooltip(tr, subtext, mx, my + 17);
     }
 
     public record State(HexCoord start, HexPattern pattern) {
