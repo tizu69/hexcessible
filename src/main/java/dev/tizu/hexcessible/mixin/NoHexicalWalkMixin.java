a1ba3c31be2336cb8fc1a47252ceab39b9a0edc9
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-10-31 21:49:49 +0100

    rename no hexical walk

--- src/main/java/dev/tizu/hexcessible/mixin/NoHexicalWalkMixin.java
+++ src/main/java/dev/tizu/hexcessible/mixin/NoHexicalWalkMixin.java
@@ -11,20 +11,24 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.input.Input;
 import net.minecraft.client.network.ClientPlayerEntity;
 
+// FIXME: this is still broken on hexxy4 :3
+// Also, some players may want to move, so removal is not an option.
+// Readd to mixin json once fixed
+
 @Mixin(ClientPlayerEntity.class)
-public class DrawStateClientPlayerEntityMixin {
+public class NoHexicalWalkMixin {
     @Shadow
     public Input input;
 
     @Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/tutorial/TutorialManager;onMovement(Lnet/minecraft/client/input/Input;)V", shift = At.Shift.AFTER))
     private void noHexicalWalk(CallbackInfo info) {
+
         MinecraftClient client = MinecraftClient.getInstance();
         if (!(client.currentScreen instanceof GuiSpellcasting))
             return;
 
         // TODO: this prevents Hexical walk from working, as it collides with
-        // our keyboard input method. We might want to implement a way to handle
-        // walking for the players who want to walk?
+        // our keyboard input method.
         input.pressingForward = false;
         input.pressingBack = false;
         input.pressingLeft = false;
