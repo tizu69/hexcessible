700378b4d099b3e5c3439867b10e0f2cf366e652
tizu69 <60812901+tizu69@users.noreply.github.com>
2025-11-01 01:54:46 +0100

    make escape smartsig contain chars that represent them in code, i.e. backslash for escape or brackets for open/close paren

--- src/main/java/dev/tizu/hexcessible/smartsig/Escape.java
+++ src/main/java/dev/tizu/hexcessible/smartsig/Escape.java
@@ -14,16 +14,18 @@ import net.minecraft.util.Identifier;
 
 public class Escape implements SmartSig {
     public static final List<PatternEntries.Entry> ALL = List.of(
-            make("escape", HexDir.WEST, "qqqaw", "hexcasting.page.patterns_as_iotas.escape.1"),
-            make("block_open", HexDir.WEST, "qqq", "hexcasting.page.patterns_as_iotas.parens.1"),
-            make("block_close", HexDir.EAST, "eee", "hexcasting.page.patterns_as_iotas.parens.2"),
-            make("undo", HexDir.EAST, "eeedw", "hexcasting.page.patterns_as_iotas.undo"));
+            make("escape", HexDir.WEST, "qqqaw", "hexcasting.page.patterns_as_iotas.escape.1", " \\"),
+            make("open_paren", HexDir.WEST, "qqq", "hexcasting.page.patterns_as_iotas.parens.1", " ({"),
+            make("close_paren", HexDir.EAST, "eee", "hexcasting.page.patterns_as_iotas.parens.2", " )}"),
+            make("undo", HexDir.EAST, "eeedw", "hexcasting.page.patterns_as_iotas.undo", " /")); // why / ???
 
-    private static PatternEntries.Entry make(String id, HexDir dir, String sig, String desc) {
+    private static PatternEntries.Entry make(String id, HexDir dir, String sig,
+            String desc, String nameadd) {
         var doc = new BookEntries.Entry("hexcessible:escape/" + id, null,
                 Text.translatable(desc).getString(), "", "", 0);
-        return new PatternEntries.Entry(Identifier.of("hexcessible", id),
-                Text.translatable("hexcessible.smartsig.escape." + id).getString(),
+        var name = Text.translatable("hexcasting.rawhook.hexcasting:" + id)
+                .getString() + nameadd;
+        return new PatternEntries.Entry(Identifier.of("hexcessible", id), name,
                 () -> false, dir, List.of(Utils.angle(sig)), List.of(doc), 0);
     }
 
